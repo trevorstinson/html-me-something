263d3bd25c849206bae7f140f2f6bb8dc5bd3ae5
Trevor Stinson <trevor@trevorstinson.com>
2018-09-14 20:00:49 -0500

    Switch to Dart Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
+'use strict';
+ 
 var gulp = require('gulp');
-
-var sass = require('gulp-sass');
-
-gulp.task('sass', function(){
-    return gulp.src('scss/*.scss')
-      .pipe(sass()) 
-      .pipe(gulp.dest('css'))
+var sass = require('gulp-dart-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
-
-// gulp.watch('scss/**/*.scss', ['sass']); 
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
